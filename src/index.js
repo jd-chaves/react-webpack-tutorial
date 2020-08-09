b62527d7a90427e502b3caad36f041848ada8b51
Juan Chaves <chavesjuan2013@gmail.com>
2020-08-09 17:31:23 -0500

    second commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MyImage from './assets/img.png';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+        <div>Welcome to my-webpack-react-starter</div>
+        <img src={MyImage} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
